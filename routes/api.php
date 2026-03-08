03108f8f5510e8ea132d963644cecb2164ae3c65
sobhymenna40@gmail.com <yourfavmoon>
2026-03-08 09:53:22 +0200

    settings

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\AuthDataController;
 use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Modules\Admin\Http\Controllers\RoleController;
 
 Route::prefix('v1/auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
@@ -18,7 +19,7 @@ Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::get('auth-data', [AuthDataController::class, '__invoke']);
 
 
-
+    Route::apiResource('roles', RoleController::class);
 
     Route::apiResource('users', UserController::class);
     Route::apiResource('settings', SettingsController::class);
