83f9372e18937aff3d013ed1fec442c29f287d79
hamdy_malik <malik@malik.malik>
2026-03-23 13:47:46 +0200

    malik

--- app/Http/Controllers/Auth/AuthDataController.php
+++ app/Http/Controllers/Auth/AuthDataController.php
@@ -3,15 +3,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AuthDataResources;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class AuthDataController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $user = User::with('roles:id,name', 'permissions')->findOrFail(Auth::id());
+        $user = $request->user()->load(['roles:id,name', 'permissions']);
 
         return new AuthDataResources($user);
 
